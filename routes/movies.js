66e69999efb4b54c69a9e0fd2fc5c3e1d602a8e8
Timofey Kafanov <tim.kafanov@gmail.com>
2022-04-25 15:11:26 +0400

    feat: add Joy in routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const { getMovies, addMovie, deleteMovie } = require('../controllers/movies');
 
 router.get('/', getMovies);
 router.post('/', addMovie);
-router.delete('/_id', deleteMovie);
+
+router.delete('/_id', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().length(24).hex(),
+  }),
+}), deleteMovie);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const { getUserInfo, updateUserInfo } = require('../controllers/users');
 
 router.get('/me', getUserInfo);
-router.patch('/me', updateUserInfo);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+  }),
+}), updateUserInfo);
 
 module.exports = router;
