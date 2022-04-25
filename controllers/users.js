1b3fa96240d84b8e693cebda851a41f29f65776a
Timofey Kafanov <tim.kafanov@gmail.com>
2022-04-25 18:08:38 +0400

    refactor: rename app.js

--- controllers/users.js
+++ controllers/users.js
@@ -99,11 +99,7 @@ const login = (req, res, next) => {
               maxAge: 3600000 * 24 * 7,
               httpOnly: true,
             })
-            .send({
-              data: {
-                user,
-              },
-            });
+            .send(user);
         })
         .catch(next);
     });
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const { getUserInfo, updateUserInfo } = require('../controllers/users');
 router.get('/me', getUserInfo);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
+    email: Joi.string().required().email(),
     name: Joi.string().min(2).max(30).required(),
   }),
 }), updateUserInfo);
