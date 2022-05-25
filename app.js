bc2aaeb33b12c4efbd3353945ec31e375e222ac1
Timofey Kafanov <tim.kafanov@gmail.com>
2022-05-25 14:36:33 +0400

    refactor: change cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -10,32 +11,51 @@ const { PORT = 3001, MONGO_ADDRESS = 'mongodb://localhost:27017/moviesdb' } = pr
 
 const app = express();
 
+const allowedCors = [
+  'localhost:3000',
+  'http://localhost:3000',
+  'http://movie.diploma.nomoreparties.sbs',
+  'https://movie.diploma.nomoreparties.sbs',
+];
+
+app.use(cors());
+
 app.use((req, res, next) => {
-  const allowedCors = [
-    'localhost:3000',
-    'http://localhost:3000',
-    'movie.diploma.nomoreparties.sbs',
-    'http://movie.diploma.nomoreparties.sbs',
-    'https://movie.diploma.nomoreparties.sbs',
-  ];
   const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
   }
 
   next();
-  return true;
 });
+
+// app.use((req, res, next) => {
+//   const allowedCors = [
+//     'localhost:3000',
+//     'http://localhost:3000',
+//     'movie.diploma.nomoreparties.sbs',
+//     'http://movie.diploma.nomoreparties.sbs',
+//     'https://movie.diploma.nomoreparties.sbs',
+//   ];
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//   }
+
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+
+//   next();
+//   return true;
+// });
 
 app.use(cookieParser());
 
